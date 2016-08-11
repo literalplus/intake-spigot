d165627715ecc2fbdd12b76387a71418177c5a33
xxyy <devnull@nowak-at.net>
2016-08-11 23:31:48 +0200

    Fix misleading copyright headers

--- src/main/java/li/l1t/common/intake/CommandsManager.java
+++ src/main/java/li/l1t/common/intake/CommandsManager.java
@@ -1,11 +1,19 @@
 /*
- * Copyright (c) 2013 - 2015 xxyy (Philipp Nowak; devnull@nowak-at.net). All rights reserved.
+ * Intake-Spigot, a Spigot bridge for the Intake command framework.
+ * Copyright (C) Philipp Nowak (Literallie)
  *
- * Any usage, including, but not limited to, compiling, running, redistributing, printing,
- *  copying and reverse-engineering is strictly prohibited without explicit written permission
- *  from the original author and may result in legal steps being taken.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * See the included LICENSE file (core/src/main/resources) or email xxyy98+xyclicense@gmail.com for details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package li.l1t.common.intake;
